cc51e63b2fa01a10ad0593a0f0e8a1b5eecf09a5
NinaRoun <nina_roun@mail.ru>
2019-04-25 13:37:29 +0700

    change Actions for Async

---
+++ build/precache-manifest.4a3bfbed04c4dc5917f25ce181342946.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e89feede18caf57ecf72",
+    "url": "/bootcamp-study-project/static/js/runtime~main.17fb8465.js"
+  },
+  {
+    "revision": "f858c7afa70dd6f36191",
+    "url": "/bootcamp-study-project/static/js/main.12c28498.chunk.js"
+  },
+  {
+    "revision": "50b8ac6b69bb7e759a73",
+    "url": "/bootcamp-study-project/static/js/2.88f7e628.chunk.js"
+  },
+  {
+    "revision": "f858c7afa70dd6f36191",
+    "url": "/bootcamp-study-project/static/css/main.a584d9ce.chunk.css"
+  },
+  {
+    "revision": "8011682b722f5f85787623a39b4d1e69",
+    "url": "/bootcamp-study-project/index.html"
+  }
+];
